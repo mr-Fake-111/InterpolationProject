30c5d5324b9f8015625cfd54fd7f2025f02674a3
Bogdan <bogdantarasason@gmail.com>
2023-03-21 22:38:32 +0300

    third commit (Newton method)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,10 @@ import src_current.*
 fun main(args: Array<String>) {
 
 
-    val properties = UserFunction.getNodes(getStandartNodes(0.0, 5*Math.PI, 6))
-    val poly = Interpolator.interpolationPolynomialLagrange(properties)
-    print(poly.toString())
+    val nodes = NodeGenerator.getStandartNodes(0.0, Math.PI, 6)
+    val properties = UserFunction.getNodes(nodes)
+    val poly = Interpolator.interpolationPolynomialSLAE(properties)
+    println(poly.toString())
+    println(Interpolator.interpolationPolynomialNewton(properties, 3.0))
+    println(Interpolator.interpolationPolynomialLagrange(properties, 3.0))
 }
--- src/main/kotlin/src_current/Interpolator.kt
+++ src/main/kotlin/src_current/Interpolator.kt
@@ -5,14 +5,9 @@ import kotlin.math.pow
 
 class Interpolator {
     companion object {
-        fun interpolationPolynomialNewton(nodes: HashMap<Double, Double>): Polynomial {
-            val newtonPolynomial = Polynomial()
 
+        fun interpolationPolynomialSLAE(nodes: Array<DoubleArray>): Polynomial {
 
-            return newtonPolynomial
-        }
-
-        fun interpolationPolynomialLagrange(nodes: Array<DoubleArray>): Polynomial {
             val n =  nodes.size
 
             var matrix = Array<DoubleArray>(n) {DoubleArray(n) }
@@ -35,7 +30,59 @@ class Interpolator {
             return Polynomial(coefficients)
         }
 
-        fun interpolationSPLine(nodes: HashMap<Double, Double>): Polynomial {
+        //данный метод не возвращает полином в явном виде, а лишь сводит вычисление сложной функции к полиномиальным вычислениям
+        //то есть возвращает значение в точке, обусловленное таблицей узлов и спецификой построения полинома
+        //я мог бы перегрузить метод, чтобы он был хоть как-то применим за пределами задания, но .... пока не судьба
+        fun interpolationPolynomialLagrange(nodes: Array<DoubleArray>, spot: Double): Double {
+            var spotVal = 0.0
+
+            for( i in 0..(nodes.size-1)) {
+                var currPart = 1.0
+                for(j in 0.. (i-1)){
+                    currPart *= (spot - nodes[j][0])/(nodes[i][0] - nodes[j][0])
+                }
+                for(j in (i+1).. (nodes.size-1)){
+                    currPart *= (spot - nodes[j][0])/(nodes[i][0] - nodes[j][0])
+                }
+                currPart *= nodes[i][1]
+                spotVal += currPart
+            }
+
+            return spotVal
+        }
+
+        //как и с предыдущим методом, в явном виде найти полином сложно, так что возвращаем только значение в точке
+        fun interpolationPolynomialNewton(nodes: Array<DoubleArray>, spot:Double): Double {
+            var spotVal = 0.0
+
+            for(k in 0..(nodes.size-1)) {
+                var currSumPart = 0.0
+
+                //использована альтернативная формула разделенной разности
+                for(i in 0..k) {
+                    var currMultPart = nodes[i][1]
+
+                    for (j in 0..(i - 1)) {
+                        currMultPart /= (nodes[i][0] - nodes[j][0])
+                    }
+                    for (j in (i + 1)..k) {
+                        currMultPart /= (nodes[i][0] - nodes[j][0])
+                    }
+                    currSumPart += currMultPart
+                }
+
+                //после чего c найденной разделенной разностью производим домножение на множители вида (x-xi), где i = 0..(k-1)
+                for(i in 0..(k-1)) {
+                    currSumPart *= (spot - nodes[i][0])
+                }
+                //добавляем слагаемое из предсталения полинома Ньютона в результирующую переменную
+                spotVal += currSumPart
+            }
+
+            return spotVal
+        }
+
+        fun interpolationSPLineLinear(nodes: HashMap<Double, Double>): Polynomial {
 
             return Polynomial()
         }
